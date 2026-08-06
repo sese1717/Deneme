3f6fd27d7b68045943088ae63138bacdb7489a2b
sese1717 <senin_github_epostan@email.com>
2026-08-06 17:16:11 +0300

    N

---
+++ Operatörler_Atama.py
@@ -0,0 +1,23 @@
+a = 10 
+b = 20
+c = 30
+
+a,b,c = 10,20,30
+
+a,b = b,a
+
+print(a,b,c)
+
+a = a + 5
+
+print(a)
+
+a += 5 # a' nın değerini 5 artırır. Diğer operatörler: -=, *=, /=, //=, **=, %= ilede yaopılabilir.
+
+print(a)
+
+sayilar = (1,2,3)
+
+a,b,c = sayilar
+
+print(a,b,c)
