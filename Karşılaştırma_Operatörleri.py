273b2e1543c20f741aea650cbd6e64806a6912ad
sese1717 <senin_github_epostan@email.com>
2026-08-07 12:23:09 +0300

    Last version

---
+++ Karşılaştırma_Operatörleri.py
@@ -0,0 +1,24 @@
+a,b,c,d = 10,10,20,5
+
+username = "Ali" 
+password = "1234"
+
+sonuc = (a == b)
+
+print(type(f"a ve b eşit mi? : {sonuc}"))
+
+sonuc = (a != b) 
+
+print(f"a ve b eşit değil mi? : {sonuc}")
+
+sonuc = (a==c)
+
+print(f"a ve c eşit mi? : {sonuc}")
+
+sonuc = (username == "Ali" and password == "1234")
+
+print(f"Kullanıcı adı ve şifre doğru mu? : {sonuc}")
+
+sonuc = (a > b)
+
+print(f"a b'den büyük mü? : {sonuc}")
