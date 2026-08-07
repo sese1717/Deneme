a87578d58c58ed606e8e5eae80e86b39e7b4321c
sese1717 <senin_github_epostan@email.com>
2026-08-07 17:35:03 +0300

    Yolla gitsin

---
+++ If_Else.py
@@ -0,0 +1,34 @@
+# sayi = -5
+
+# if (sayi > 0):
+#     print("Sayi pozitiftir.")
+# elif (sayi < 0): #Else de kullanılabilir. Else if yerine elif kullanılır.
+#     print("Sayi negatiftir.")
+
+# if (sayi > 0):
+#     print("Sayi pozitiftir.")
+# else:
+#     print("Sayi negatiftir.")
+
+# giris = True
+
+# if (giris ==True):#Gırısın içinde True olduğu için if zaten çalıştırıcaktı.Bu yüzden ==True yazmamıza gerek yoktu.
+#     print("Giriş başarılı.")
+# else:
+#     print("Giriş başarısız.")
+
+username= "Selim Emir OCAK"
+password= "123456"
+
+# if (username == "Selim Emir OCAK" and password == "12345"):
+#     print("Giris başarılı.")
+# else:
+#     print("Giris başarısız.")
+
+if(username == "Selim Emir OCAK"):
+    if (password == "12345"):
+        print("Giris başarılı.")
+    else:
+        print("Parola hatalı.")
+else:
+    print("Kullanıcı adı ve parola hatalı.")
