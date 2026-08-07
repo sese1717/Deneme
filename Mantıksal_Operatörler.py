80ac93110563e8633100d048c8f33b3c9ee66a62
sese1717 <senin_github_epostan@email.com>
2026-08-07 12:36:34 +0300

    A

---
+++ Mantıksal_Operatörler.py
@@ -0,0 +1,33 @@
+# And operatörü
+# x = 15
+# sonuc = 10 < x < 20
+
+# print(f"10 < x < 20 : {sonuc}")
+
+# sonuc = (x > 10 and x < 20)
+
+# print(f"x > 10 and x < 20 : {sonuc}")
+
+# karnenotu = 50
+# devamsızlık = 10
+
+# sonuc = (karnenotu >= 50 and devamsızlık <= 10)
+
+# print(f"Geçti mi? : {sonuc}")
+
+# Or operatörü(veya)
+
+a = 15
+b = 10
+c = 20
+
+sonuc = (a > b or a < c)
+
+print(f"a b'den büyük mü veya a c'den küçük mü? : {sonuc}")
+
+sonuc = not (a > b or a < c)
+
+print(f"a b'den büyük mü veya a c'den küçük mü? : {sonuc}")
+
+
+
