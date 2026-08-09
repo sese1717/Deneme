cee49fed6490b83993858261dbf653e6b97db61a
sese1717 <senin_github_epostan@email.com>
2026-08-09 21:41:55 +0300

    A

---
+++ Break_Continue.py
@@ -0,0 +1,15 @@
+isim = "Selim Emir OCAK"
+
+# for harf in isim:
+#     if(harf=="E"):
+#         break
+#     print(harf)
+
+i = 0
+
+while (i < 10):
+    i += 1
+    if ( i==5 ):
+        continue
+    print(i)
+    
