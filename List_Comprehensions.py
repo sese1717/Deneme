f47aeb8c39662702571ed92799d889542919e8f7
sese1717 <senin_github_epostan@email.com>
2026-08-11 19:57:12 +0300

    M

---
+++ List_Comprehensions.py
@@ -0,0 +1,16 @@
+# sayilar = []
+
+# for i in range(1,11):
+#     sayilar.append(i)
+# print(sayilar)
+
+#expression for item in list
+
+sayilar = [i*2 for i in range(10)]
+print(sayilar)
+
+liste = [3,8,5,12,40]
+
+sayilar = [i*2 for i in liste]
+
+print(sayilar)
