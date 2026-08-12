9ef6c39c937b9f8131c287ae24d3a63716bae1ae
sese1717 <senin_github_epostan@email.com>
2026-08-12 14:22:53 +0300

    A

--- Fonksiyon_Tanımlama.py
+++ Fonksiyon_Tanımlama.py
@@ -1,13 +1,42 @@
-def karsilama():
-    for i in range(10):
-        print("Merhaba")
+# def karsilama():
+#     for i in range(10):
+#         print("Merhaba")
 
-karsilama()
+# karsilama()
 
-a = 5
-b = 10
+# a = 5
+# b = 10
 
-def carpma():
-    print(a*b)
+# def carpma():
+#     print(a*b)
 
-carpma()
+# carpma()
+
+# Return
+
+# def adin_ne():
+#     ad = input("Adınızı giriniz: ")
+#     return ad
+
+# # adin_ne()
+
+# print("Sisteme hoş geldniniz",adin_ne())
+
+# def topla():
+#     return( 20 + 30 )
+
+# sonuc = topla()*2
+
+# print(sonuc)
+
+saat = 20
+
+def selamla():
+    if saat < 12 :
+        return ("Günaydın")
+    elif saat >= 12 and saat < 18 :
+        return "İyi Günler"
+    else:
+        return "iyi akşamlar"
+
+print(selamla())
---
+++ Fonksiyona_Parametre_Göndermek.py
@@ -0,0 +1,19 @@
+
+
+def topla(a,b):
+    return a + b
+
+print(topla(20,10))
+
+print(topla(100,300))
+
+def yashesapla(dogumyılı):
+    return 2026 - dogumyılı
+
+print(yashesapla(2003))
+
+def selamla(isim):
+    return("MERHABA, "+isim)
+
+print(selamla("Selim"))
+
