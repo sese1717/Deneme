a80ba819e36c141e5dd957690c1fdbbdf43d178a
sese1717 <senin_github_epostan@email.com>
2026-08-14 13:27:47 +0300

    A

---
+++ Keywords.py
@@ -0,0 +1,7 @@
+def fullname(ad,soyad):
+    return f"Sisteme hoş geldiniz, {ad} {soyad}."
+
+# sonuc = fullname("Selim Emir","OCAK")
+sonuc = fullname(soyad="OCAK",ad="Selim Emir")
+print(sonuc)
+
---
+++ Varsayılan_Parametreler.py
@@ -0,0 +1,29 @@
+# def selamla (isim = "Kullanıcı",mesaj = "Hoş geldiniz"):
+#     return(f"{mesaj} {isim}.")
+    
+
+
+# # selamla("Mehmet","Merhaba")
+# # selamla()
+# # selamla("Levent","İyi akşamlar")
+
+# selamla()
+
+# def usAlma(taban ,us):
+#     return(taban**us)
+
+# sonuc = usAlma()
+
+# print(sonuc)
+
+def carpma(a,b):
+    return(a*b)
+
+def toplama(a,b):
+    return(a+b)
+
+def islem(a,b):
+    return(toplama(a,b))
+
+sonuc = islem(10,5)
+print(sonuc)
